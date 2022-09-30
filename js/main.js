d826a2a59266fb5050ec36fe4e98c63a3d5e4e83
Jonatas Barbosa <jonh_stars8@hotmail.com>
2022-09-29 21:16:37 -0300

    definiçoes

---
+++ js/main.js
@@ -0,0 +1,8 @@
+jQuery(function($){
+    $("#forme").on("submit",function(e){
+      e.preventDefault(); // impedir o evento submit
+  
+      // aqui entra o codigo ajax. pesquisar no google "jquery ajax" para mais detalhes
+  
+    });
+  });
